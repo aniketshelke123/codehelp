aff786926a9201fdb70af4d95f022bd185ec63e9
Aniket Shelke <aniketshelke5151@gmail.com>
2022-12-31 18:25:24 +0530

    31/12/2022

---
+++ DectoBin.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main(){
+	int n ;
+	cin >> n;
+
+	int ans = 0;
+	int i = 0;
+	while(n != 0){
+
+		//n&1 give us the last digit 0 or 1
+		int bit = n & 1;
+
+		ans = (bit * pow(10, i)) + ans;
+
+		//after noting last bit we need to shift 'n' right
+		n = n >> 1;
+		i++;
+	}
+	cout << "ANSWER IS: " << ans << endl;
+	return 0;
+}
